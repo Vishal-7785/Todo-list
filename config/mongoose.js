88e1303653b68581407b63d10403d21be324a732
Vishal Jaiswal <vishaljaiswal@Vishals-MacBook-Pro.local>
2020-10-29 14:10:21 +0530

    Adding animations

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todo_app_db');
+// Acquiring the connection to check if it was successfull
+const db = mongoose.connection;
+// if errror is there
+db.on('errror',console.error.bind(console,'Error connecting to database'));
+// If connection Successfull
+db.once('open',function(){
+    console.log('Successfully connected to databse');
+});
--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 const express = require('express');
 const port = 9000;
+const path = require('path');
 const app = express();
+const db = require('./config/mongoose');
+const Todo = require('./models/todo');
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'))
+
+app.get('/',function(req,res){
+    Todo.find({},function(err,todo){
+        if(err){
+            console.log(`Error in ruuning the server : ${err}`);
+            return;
+        }
+        return res.render('home',{
+            title: "My Todo APP",
+            TODO: todo
+            });
+    });
+
+});
+ app.get('/profile',function(req,res){
+    return res.render('profile',{
+        title: "Todo profile"
+    });
+ });
+ app.post('/create-work',function(req,res){
+    Todo.create({
+        task: req.body.task,
+        date: req.body.date,
+        category: req.body.category
+    }, function(err,newTask){
+        if(err){
+            console.log(`Error in creating a task: ${err}`);
+        }
+        console.log('*****',newTask);
+        return res.redirect('back');
+
+    });
+});
+app.get('/delete/work/',function(req,res){
+      Todo.findByIdAndDelete(req.query.id,function(err){
+          if(err){
+              console.log(`Error in deleting task: ${err}`);
+              return res.redirect('back');
+          }
+          return res.redirect('back');
+      });
+});
 
 app.listen(port,function(err){
     if(err){
